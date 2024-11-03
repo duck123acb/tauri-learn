fa7645a6c59f383d50e918851029d53595eef5e2
Danush Ramanan <dan.ramanan@gmail.com>
2024-11-03 13:32:42 -0700

    can delete items now

--- src/main.js
+++ src/main.js
@@ -1,30 +1,45 @@
 const { invoke } = window.__TAURI__.core;
 
 let remindersContainerEl;
+let newInputEl;
 let reminderElInstance;
-let newInput;
 
-async function addItem(msg) {
-  if (msg.trim() === "") {
+// greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+async function addItem(inputElement) {
+  let msg = inputElement.value;
+  if (msg.trim() === "") { // if the reminder is empty, dont consider it
     return;
   }
-  console.log(msg);
-  // greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+
+  let newReminder = reminderElInstance.cloneNode(true);
+  newReminder.querySelector(".item").innerText = msg; // change the reminder's text to be the text the user wants
+  remindersContainerEl.appendChild(newReminder); // add the reminder to the DOM
+  newReminder.querySelector(".done-btn").addEventListener("click", (e) => { // the click function doesnt bind unless its been added to the DOM first
+    deleteItem(e.target.parentElement);
+  });
+
+  inputElement.value = ""; // reset the input box
+}
+async function deleteItem(reminderElement) {
+  reminderElement.remove();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   remindersContainerEl = document.querySelector("#reminders-container");
-  reminderElInstance = document.querySelector(".reminder-container").cloneNode(true);
+  newInputEl = document.querySelector("#new-input");
+
+  reminderElInstance = document.querySelector(".reminder-container").cloneNode(true); // gets an instance of the hidden element
   reminderElInstance.classList.remove("hidden");
-  newInput = document.querySelector("#new-input");
-  newInput.addEventListener("keydown", (e) => {
+
+  // adding items
+  newInputEl.addEventListener("keydown", (e) => {
     if (e.key !== "Enter") {
       return;
     }
 
-    addItem(newInput.value);
+    addItem(newInputEl);
   });
   document.querySelector("#new-btn").addEventListener("click", () => {
-    addItem(newInput.value);
+    addItem(newInputEl);
   });
 });
